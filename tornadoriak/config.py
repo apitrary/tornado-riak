9e75cdb63c26b82f9e75178b1f9a92e14543f7b7
Hans-Gunther Schmidt <hgs@apitrary.com>
2013-06-29 19:31:53 +0200

    removing cookie secret

--- tornadoriak/config.py
+++ tornadoriak/config.py
@@ -19,8 +19,7 @@ APP_DETAILS = {
 ILLEGAL_ATTRIBUTES_SET = ['_createdAt', '_updatedAt', '_init']
 ILLEGAL_CHARACTER_SET = ['_', '__']
 
-COOKIE_SECRET = 'foh<Fei^joY(oo0hoh8eih4jumahx9toothahlah7eidieshei7ophiew7Oor1zudah^beefei1Zei;rauNeeTh9pu?wai3Ra'
 TORNADO_APP_SETTINGS = {
-    'cookie_secret': COOKIE_SECRET,
+    'cookie_secret': 'PUT_YOUR_SECRET_HERE',
     'xheaders': True
 }
